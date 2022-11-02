01db59ca7efadf7f4ac61ef72320694ac28516d1
glinkinzhenya <glinkinzhenya@gmail.com>
2022-11-02 13:47:02 +0200

    1

---
+++ script.js
@@ -0,0 +1,15 @@
+numberOne = +prompt("введи номер");
+numberTwo = +prompt("еще одно");
+alert(numberOne + numberTwo);
+
+numberOne = prompt("введи номер");
+numberTwo = prompt("еще одно");
+alert(numberOne - numberTwo);
+
+numberOne = prompt("введи номер");
+numberTwo = prompt("еще одно");
+alert(numberOne * numberTwo);
+
+numberOne = prompt("введи номер");
+numberTwo = prompt("еще одно");
+alert(numberOne / numberTwo);
