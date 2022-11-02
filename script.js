5b6722d0a670fd348001f841e9ab779fbd1c279d
glinkinzhenya <glinkinzhenya@gmail.com>
2022-11-02 14:26:48 +0200

    2

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+// numberOne = +prompt("введи номер");
+// numberTwo = +prompt("еще одно");
+// alert(numberOne + numberTwo);
+
+// numberOne = prompt("введи номер");
+// numberTwo = prompt("еще одно");
+// alert(numberOne - numberTwo);
+
+// numberOne = prompt("введи номер");
+// numberTwo = prompt("еще одно");
+// alert(numberOne * numberTwo);
+
 numberOne = +prompt("введи номер");
 numberTwo = +prompt("еще одно");
-alert(numberOne + numberTwo);
-
-numberOne = prompt("введи номер");
-numberTwo = prompt("еще одно");
-alert(numberOne - numberTwo);
-
-numberOne = prompt("введи номер");
-numberTwo = prompt("еще одно");
-alert(numberOne * numberTwo);
-
-numberOne = prompt("введи номер");
-numberTwo = prompt("еще одно");
-alert(numberOne / numberTwo);
+alert(`${numberOne} + ${numberTwo} = ${numberOne + numberTwo}`);
+alert(`${numberOne} - ${numberTwo} = ${numberOne - numberTwo}`);
+alert(`${numberOne} * ${numberTwo} = ${numberOne * numberTwo}`);
+alert(`${numberOne} / ${numberTwo} = ${numberOne / numberTwo}`);
